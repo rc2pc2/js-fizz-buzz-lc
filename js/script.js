f1ea2b280ecfe309e1ece1b636dba61460dbf1c5
rc2pc2 <petriccarcc@gmail.com>
2023-11-15 10:53:47 +0100

    fizzbuzz dom final review

--- js/script.js
+++ js/script.js
@@ -5,26 +5,25 @@ const wrapperElement = document.querySelector('div.wrapper');
 // multiplo significa che e' divisibile per quel numero senza resto
 // # a % b = resto => 6 % 3 =? 0
 
-
 for (let i = 1; i <= 100 ; i++ ){
     const fizzBuzzElement = document.createElement('article');
     fizzBuzzElement.className = 'card';
 
+    fizzBuzzElement.addEventListener('click', function(){
+        fizzBuzzElement.classList.toggle('active');
+    });
+
     if ( i % 15 === 0){
         fizzBuzzElement.append('fizzbuzz');
-        // % fizzBuzzElement.className += ' bg-yellow';
         fizzBuzzElement.classList.add('bg-yellow');
     } else if (i % 3 === 0){
         fizzBuzzElement.append('fizz');
-        // % fizzBuzzElement.className += ' bg-green';
         fizzBuzzElement.classList.add('bg-green');
     } else if (i % 5 === 0) {
         fizzBuzzElement.append('buzz');
-        // % fizzBuzzElement.className += ' bg-cyan';
         fizzBuzzElement.classList.add('bg-cyan');
     } else {
         fizzBuzzElement.append(i);
-        // % fizzBuzzElement.className += ' bg-blue';
         fizzBuzzElement.classList.add('bg-blue');
     }
 
