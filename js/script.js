683e810819e4d12257dc14e6600e13711aa172e2
rc2pc2 <petriccarcc@gmail.com>
2023-11-15 10:07:22 +0100

    fizzbuzz dom with classList.add

--- js/script.js
+++ js/script.js
@@ -2,18 +2,31 @@
 // ? recupero l'elemento che sara' il parent di quello che voglio popolare nell'html
 const wrapperElement = document.querySelector('div.wrapper');
 
-
 // multiplo significa che e' divisibile per quel numero senza resto
 // # a % b = resto => 6 % 3 =? 0
 
+
 for (let i = 1; i <= 100 ; i++ ){
+    const fizzBuzzElement = document.createElement('article');
+    fizzBuzzElement.className = 'card';
+
     if ( i % 15 === 0){
-        console.log('FuzzBuzz');
+        fizzBuzzElement.append('fizzbuzz');
+        // % fizzBuzzElement.className += ' bg-yellow';
+        fizzBuzzElement.classList.add('bg-yellow');
     } else if (i % 3 === 0){
-        console.log('Fizz');
+        fizzBuzzElement.append('fizz');
+        // % fizzBuzzElement.className += ' bg-green';
+        fizzBuzzElement.classList.add('bg-green');
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        fizzBuzzElement.append('buzz');
+        // % fizzBuzzElement.className += ' bg-cyan';
+        fizzBuzzElement.classList.add('bg-cyan');
     } else {
-        console.log(i);
+        fizzBuzzElement.append(i);
+        // % fizzBuzzElement.className += ' bg-blue';
+        fizzBuzzElement.classList.add('bg-blue');
     }
+
+    wrapperElement.appendChild(fizzBuzzElement);
 }
